1cdeb7b0ea79dfd967f3e5564fa9b38922b30f46
Aristocrab <maksymkanupa@gmail.com>
2025-07-02 15:12:26 +0300

    Update TodoTask.cs

--- AspireTodoApp.ApiService/Models/TodoTask.cs
+++ AspireTodoApp.ApiService/Models/TodoTask.cs
@@ -1,3 +1,19 @@
 namespace AspireTodoApp.ApiService.Models;
 
-public record TodoTask(Guid Id, string Title, string? Description, Status Status, DateTime CreatedAt);
+public class TodoTask
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public Status Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public TodoTask(Guid id, string title, string? description, Status status, DateTime createdAt)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Status = status;
+        CreatedAt = createdAt;
+    }
+}
